2e51aae9b29af6a54ff61f11b5391b92af568681
Adolfo Solis <asolis@polyworksmexico.com>
2022-03-30 16:36:35 -0600

    Updated README.md

--- Stereo Depth Perception/README.md
+++ Stereo Depth Perception/README.md
@@ -1,1 +1,65 @@
 # Stereo Depth Perception - CUDA
+
+Computer stereo vision is the process of extracting three-dimensional information from digital images
+by calculating depth based on the binocular discrepancy between the left and right camera images of
+an object. In stereo vision, two cameras located on the same horizontal line are displaced relative to
+each other, which allow you to get one image from two points, this works similarly to the binocular
+vision of a person.
+
+Analysis of these two images allows obtaining disparity map information. A
+disparity map is an indicator of the difference in the relative position of the same points recorded by
+two cameras. This map allows us to calculate the difference in horizontal coordinates of the
+corresponding points of the image, which ultimately will allow us to calculate the distance to the
+object.
+
+## Requirements
+
+### - OS: Windows or Linux.
+
+### - OpenCV 4+.
+
+### - CUDA capable GPU.
+
+### - Dataset _(Provided)_ or Stereo Camera.
+
+## Wrapper
+
+```cpp
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+namespace StereoDepthPerceptionLib
+{
+    // Call this once.
+    // Size = Size(width, height)
+	void Setup(const cv::Size size);
+
+    // Call this every frame.
+	void Compute(
+		const cv::Mat &leftImage,
+		const cv::Mat &rightImage);
+
+    // Call this every frame.
+	void GetDepthImage(cv::Mat &depthImage);
+}
+```
+
+## Demo
+
+**1. Create the build folder.**
+
+```command
+    mkdir build && cd build
+```
+
+**2. Cmake and make.**
+
+```command
+    cmake .. && make
+```
+
+**3. Run the demo.**
+
+```command
+    ./StereoDepthPerception
+```
--- Stereo Depth Perception/main.cpp
+++ Stereo Depth Perception/main.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "lib/plugin.h"
 
+cv::Ptr<cv::StereoBM> m_StereoBM;
+
 VOID DP_Process(
 	CONST IN cv::Mat &LeftImage,
 	CONST IN cv::Mat &RightImage,
@@ -10,13 +12,24 @@ VOID DP_Process(
 	StereoDepthPerceptionLib::GetDepthImage(DepthImage);
 }
 
+VOID OCV_Process(
+	CONST IN cv::Mat &LeftImage,
+	CONST IN cv::Mat &RightImage,
+	OUT cv::Mat &DisparityImage)
+{
+	cv::Mat GL, GR;
+	cv::cvtColor(LeftImage, GL, cv::COLOR_BGR2GRAY);
+	cv::cvtColor(RightImage, GR, cv::COLOR_BGR2GRAY);
+	m_StereoBM->compute(GL, GR, DisparityImage);
+	cv::normalize(DisparityImage, DisparityImage, 0, 255, cv::NORM_MINMAX, CV_8U);
+}
+
 int main()
 {
 	std::string DATASET_PATH = "../res/";
 	cv::VideoCapture cam0, cam1, cam2;
 	cam0 = cv::VideoCapture(DATASET_PATH + "image_L/image (1).png", cv::CAP_IMAGES);
 	cam1 = cv::VideoCapture(DATASET_PATH + "image_R/image (1).png", cv::CAP_IMAGES);
-	cam2 = cv::VideoCapture(DATASET_PATH + "disparity/image (1).png", cv::CAP_IMAGES);
 
 	std::cout << "Reading Dataset: " + DATASET_PATH << std::endl;
 	if (!cam0.isOpened() || !cam1.isOpened())
@@ -25,24 +38,27 @@ int main()
 		return -1;
 	}
 
+	// OpenCV
+	m_StereoBM = cv::StereoBM::create();
+
+	// CUDA
 	StereoDepthPerceptionLib::Setup(cv::Size(cam0.get(cv::CAP_PROP_FRAME_WIDTH), cam0.get(cv::CAP_PROP_FRAME_HEIGHT)));
 
 	cv::Mat LeftImage, RightImage, DepthImage, DisparityImage;
-	while (cam0.read(LeftImage) && cam1.read(RightImage) && cam2.read(DisparityImage))
+	while (cam0.read(LeftImage) && cam1.read(RightImage))
 	{
-		DisparityImage.convertTo(DisparityImage, CV_8UC1);
-		cv::applyColorMap(DisparityImage, DisparityImage, cv::COLORMAP_WINTER);
-
 		auto TIME_START;
 		DP_Process(LeftImage, RightImage, DepthImage);
 		auto TIME_STOP;
+
+		OCV_Process(LeftImage, RightImage, DisparityImage);
 
 		cv::Mat StereoImage;
 		cv::hconcat(LeftImage, RightImage, StereoImage);
 		cv::putText(StereoImage, "FPS: " + std::to_string(1 / TIME_RESULT), {10, 15}, 1, 1, CV_RGB(255, 0, 0));
 		cv::imshow("Stereo WebCam", StereoImage);
 		cv::imshow("Depth(CUDA)", DepthImage);
-		cv::imshow("Disparity(Dataset)", DisparityImage);
+		cv::imshow("Disparity(Open CV)", DisparityImage);
 		cv::waitKey(1);
 	}
 
